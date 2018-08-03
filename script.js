0eb378f9bad6a7af02d60096c069a9c12a58198c
William Connelly <williamconnelly.dev@gmail.com>
2018-08-03 08:23:59 -0700

    Reorganized tech icons, toying with hover events

--- script.js
+++ script.js
@@ -4,10 +4,11 @@ var addIconHover = () => {
   const landingIcons = Array.from(document.getElementsByClassName("landing-icon"));
   landingIcons.forEach(icon => {
     icon.addEventListener("mouseover", () =>{
-      icon.style.color = "lightblue";
+      icon.classList.add("hoveredIcon");
     })
     icon.addEventListener("mouseout", () =>{
       console.log("OUT");
+      icon.classList.remove("hoveredIcon");
     });
   });
 };
